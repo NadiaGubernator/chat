ad110c58dc4d39ec2642e6eeb8f161c6d3a286b7
Nadia Gubernator <nadia.gubernator@gmail.com>
2017-12-28 21:54:27 +0200

    Finish first part of nopio tutorial

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root       'home#index'
+
+  devise_for :users
+
+  resources :messages,      only: %i[create]
+  resources :conversations, only: %i[create] do
+    member do
+      post :close
+    end
+  end
 end
