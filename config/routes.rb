c57ffd081057cc164676239f81200b6c931ef800
Nadia Gubernator <nadia.gubernator@gmail.com>
2017-12-29 00:09:19 +0200

    Finish second part of nopio tutorial

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   root       'home#index'
 
   devise_for :users
-
-  resources :messages,      only: %i[create]
+  
   resources :conversations, only: %i[create] do
     member do
       post :close
